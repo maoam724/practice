c22d7e8bf15eb74fde924b948419d099c9304446
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-08 23:34:09 +0900

    直しといたよー

--- recipe/recipe/AddRecipeViewController.swift
+++ recipe/recipe/AddRecipeViewController.swift
@@ -1,9 +1,9 @@
 //
 //  AddRecipeViewController.swift
-//  
+//  recipe
 //
 //  Created by mao takagai on 2017/07/08.
-//
+//  Copyright © 2017 mao takagai. All rights reserved.
 //
 
 import UIKit
