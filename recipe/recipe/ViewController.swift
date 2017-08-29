82138e35615c02285a3756aca29cc69704a93453
maoam724 <maoam.724@gmail.com>
2017-08-30 00:17:56 +0900

    レシピのリスト表示させたよ〜

--- recipe/recipe/ViewController.swift
+++ recipe/recipe/ViewController.swift
@@ -11,19 +11,24 @@ import JTAppleCalendar
 import RealmSwift
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recipiesTable: UITableView!
     
     let formatter = DateFormatter()
     var recipes: Results<Recipe>!
-    var recipesForDate: List<Recipe>!
+    var recipesForDate: Array<Recipe>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        recipiesTable.delegate = self
+        recipiesTable.dataSource = self
+        
         recipes = loadRecipes()
-//        recipesForDate = recipes.filter { (recipe:Recipe) -> Bool in
-//            Calendar.current.isDate(recipe.date, inSameDayAs: Date())
-//        }
+        recipesForDate = Array(recipes.filter { (recipe) -> Bool in
+            Calendar.current.isDate(recipe.date, inSameDayAs: Date())
+        })
     }
     
+    //realmから読み込む
     func loadRecipes() -> Results<Recipe> {
         let realm = try! Realm()
         let recipes = realm.objects(Recipe.self)
@@ -39,12 +44,14 @@ class ViewController: UIViewController {
     }
 }
 
+//カレンダー関連
 extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
         formatter.dateFormat = "yyyy MM dd"
         formatter.timeZone = Calendar.current.timeZone
         formatter.locale = Calendar.current.locale
         
+        //表示させる日付の範囲
         let startDate = formatter.date(from: "2017 01 01")!
         let endDate = formatter.date(from: "2017 12 31")!
         
@@ -68,21 +75,27 @@ extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSo
         cell.label.textColor = .red
         return cell
     }
-    
+
     func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
         print("押されたよ〜" + String(describing: date))
+        recipesForDate = Array(recipes.filter { (recipe) -> Bool in
+            Calendar.current.isDate(recipe.date, inSameDayAs: date)
+        })
+        recipiesTable.reloadData()
     }
 }
 
+//選択された日付に紐付くレシピを表示する
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "tableCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell")!
+        let recipe = recipesForDate[indexPath.row]
+        cell.textLabel?.text = recipe.name
+        return cell
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return recipes.filter { (recipe) -> Bool in
-            Calendar.current.isDate(recipe.date, inSameDayAs: Date())
-        }
-        .count
+        return recipesForDate.count
     }
     
 }
